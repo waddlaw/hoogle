a94553604d6db63a84b178195745fd524db715e8
waddlaw <ingronze@gmail.com>
2017-11-16 21:18:26 +0900

    hindent 適用

--- src/Action/Search.hs
+++ src/Action/Search.hs
@@ -1,29 +1,32 @@
-{-# LANGUAGE TupleSections, RecordWildCards, ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections, RecordWildCards, ScopedTypeVariables
+  #-}
 
 module Action.Search
-    (actionSearch, withSearch, search
-    ,targetInfo
-    ,targetResultDisplay
-    ,action_search_test
-    ) where
+  ( actionSearch
+  , withSearch
+  , search
+  , targetInfo
+  , targetResultDisplay
+  , action_search_test
+  ) where
 
-import Control.Monad.Extra
 import Control.DeepSeq
+import Control.Monad.Extra
+import Data.Functor.Identity
+import Data.List.Extra
 import Data.Maybe
 import qualified Data.Set as Set
-import Data.List.Extra
-import Data.Functor.Identity
 import System.Directory
 
-import Output.Items
-import Output.Tags
-import Output.Names
-import Output.Types
-import General.Store
-import Query
-import Input.Item
 import Action.CmdLine
+import General.Store
 import General.Util
+import Input.Item
+import Output.Items
+import Output.Names
+import Output.Tags
+import Output.Types
+import Query
 
 -- -- generate all
 -- @tagsoup -- generate tagsoup
@@ -31,89 +34,106 @@ import General.Util
 -- filter -- search all
 
 actionSearch :: CmdLine -> IO ()
-actionSearch Search{..} = replicateM_ repeat_ $ -- deliberately reopen the database each time
-    withSearch database $ \store ->
-        if null compare_ then do
-            (q, res) <- return $ search store $ parseQuery $ unwords query
-            whenLoud $ putStrLn $ "Query: " ++ unescapeHTML (renderQuery q)
-            let (shown, hidden) = splitAt count $ nubOrd $ map (targetResultDisplay link) res
-            if null res then
-                putStrLn "No results found"
-             else if info then do
-                 putStr $ targetInfo $ head res
-             else do
-                let toShow = if numbers && not info then addCounter shown else shown
-                putStr $ unlines toShow
-                when (hidden /= []) $ do
-                    whenNormal $ putStrLn $ "-- plus more results not shown, pass --count=" ++ show (count+10) ++ " to see more"
-        else do
-            let parseType x = case parseQuery x of
-                                  [QueryType t] -> (pretty t, hseToSig t)
-                                  _ -> error $ "Expected a type signature, got: " ++ x
-            putStr $ unlines $ searchTypesDebug store (parseType $ unwords query) (map parseType compare_)
+actionSearch Search {..} =
+  replicateM_ repeat_ $ -- deliberately reopen the database each time
+  withSearch database $ \store ->
+    if null compare_
+      then do
+        (q, res) <- return $ search store $ parseQuery $ unwords query
+        whenLoud $ putStrLn $ "Query: " ++ unescapeHTML (renderQuery q)
+        let (shown, hidden) = splitAt count $ nubOrd $ map (targetResultDisplay link) res
+        if null res
+          then putStrLn "No results found"
+          else if info
+                 then do
+                   putStr $ targetInfo $ head res
+                 else do
+                   let toShow =
+                         if numbers && not info
+                           then addCounter shown
+                           else shown
+                   putStr $ unlines toShow
+                   when (hidden /= []) $ do
+                     whenNormal $
+                       putStrLn $
+                       "-- plus more results not shown, pass --count=" ++
+                       show (count + 10) ++ " to see more"
+      else do
+        let parseType x =
+              case parseQuery x of
+                [QueryType t] -> (pretty t, hseToSig t)
+                _ -> error $ "Expected a type signature, got: " ++ x
+        putStr $
+          unlines $ searchTypesDebug store (parseType $ unwords query) (map parseType compare_)
 
 -- | Returns the details printed out when hoogle --info is called
 targetInfo :: Target -> String
-targetInfo Target{..} = 
-    unlines $ [ unHTML targetItem ] ++
-              [ unwords packageModule | not $ null packageModule] ++
-              [ unHTML targetDocs ]
-            where packageModule = map fst $ catMaybes [targetPackage, targetModule]
+targetInfo Target {..} =
+  unlines $
+  [unHTML targetItem] ++ [unwords packageModule | not $ null packageModule] ++ [unHTML targetDocs]
+  where
+    packageModule = map fst $ catMaybes [targetPackage, targetModule]
 
 -- | Returns the Target formatted as an item to display in the results
 -- | Bool argument decides whether links are shown
 targetResultDisplay :: Bool -> Target -> String
-targetResultDisplay link Target{..} = unHTML $ unwords $
-        fmap fst (maybeToList targetModule) ++
-        [targetItem] ++
-        ["-- " ++ targetURL | link]
+targetResultDisplay link Target {..} =
+  unHTML $
+  unwords $ fmap fst (maybeToList targetModule) ++ [targetItem] ++ ["-- " ++ targetURL | link]
 
 addCounter :: [String] -> [String]
-addCounter = zipWith (\i x -> show i ++ ") " ++ x) [1..]
+addCounter = zipWith (\i x -> show i ++ ") " ++ x) [1 ..]
 
 withSearch :: NFData a => FilePath -> (StoreRead -> IO a) -> IO a
 withSearch database act = do
-    unlessM (doesFileExist database) $ do
-        exitFail $ "Error, database does not exist (run 'hoogle generate' first)\n" ++
-                   "    Filename: " ++ database
-    storeReadFile database act
-
+  unlessM (doesFileExist database) $ do
+    exitFail $
+      "Error, database does not exist (run 'hoogle generate' first)\n" ++
+      "    Filename: " ++ database
+  storeReadFile database act
 
 search :: StoreRead -> [Query] -> ([Query], [Target])
-search store qs = runIdentity $ do
-    (qs, exact, filt, list) <- return $ applyTags store  qs
-    is <- case (filter isQueryName qs, filter isQueryType qs) of
-        ([], [] ) -> return list
+search store qs =
+  runIdentity $ do
+    (qs, exact, filt, list) <- return $ applyTags store qs
+    is <-
+      case (filter isQueryName qs, filter isQueryType qs) of
+        ([], []) -> return list
         ([], t:_) -> return $ searchTypes store $ hseToSig $ fromQueryType t
-        (xs, [] ) -> return $ searchNames store exact $ map fromQueryName xs
+        (xs, []) -> return $ searchNames store exact $ map fromQueryName xs
         (xs, t:_) -> do
-            nam <- return $ Set.fromList $ searchNames store exact $ map fromQueryName xs
-            return $ filter (`Set.member` nam) $ searchTypes store $ hseToSig $ fromQueryType t
+          nam <- return $ Set.fromList $ searchNames store exact $ map fromQueryName xs
+          return $ filter (`Set.member` nam) $ searchTypes store $ hseToSig $ fromQueryType t
     let look = lookupItem store
     return (qs, map look $ filter filt is)
 
-
 action_search_test :: Bool -> FilePath -> IO ()
-action_search_test sample database = testing "Action.Search.search" $ withSearch database $ \store -> do
+action_search_test sample database =
+  testing "Action.Search.search" $
+  withSearch database $ \store -> do
     let noResults a = do
           res <- return $ snd $ search store (parseQuery a)
           case res of
-              [] -> putChar '.'
-              _ -> error $ "Searching for: " ++ show a ++ "\nGot: " ++ show (take 1 res) ++ "\n expected none"
+            [] -> putChar '.'
+            _ ->
+              error $
+              "Searching for: " ++ show a ++ "\nGot: " ++ show (take 1 res) ++ "\n expected none"
     let a ==$ f = do
-            res <- return $ snd $ search store (parseQuery a)
-            case res of
-                Target{..}:_ | f targetURL -> putChar '.'
-                _ -> error $ "Searching for: " ++ show a ++ "\nGot: " ++ show (take 1 res)
+          res <- return $ snd $ search store (parseQuery a)
+          case res of
+            Target {..}:_
+              | f targetURL -> putChar '.'
+            _ -> error $ "Searching for: " ++ show a ++ "\nGot: " ++ show (take 1 res)
     let a === b = a ==$ (== b)
     let hackage x = "https://hackage.haskell.org/package/" ++ x
-    if sample then do
+    if sample
+      then do
         "__prefix__" === "http://henry.com?too_long"
         "__suffix__" === "http://henry.com?too_long"
         "__infix__" === "http://henry.com?too_long"
         "Wife" === "http://eghmitchell.com/Mitchell.html#a_wife"
-        completionTags store `testEq` ["set:all","package:emily","package:henry"]
-     else do
+        completionTags store `testEq` ["set:all", "package:emily", "package:henry"]
+      else do
         "base" === hackage "base"
         "Prelude" === hackage "base/docs/Prelude.html"
         "map" === hackage "base/docs/Prelude.html#v:map"
@@ -127,8 +147,10 @@ action_search_test sample database = testing "Action.Search.search" $ withSearch
         ">>=" === hackage "base/docs/Prelude.html#v:-62--62--61-"
         "sequen" === hackage "base/docs/Prelude.html#v:sequence"
         "foldl'" === hackage "base/docs/Data-List.html#v:foldl-39-"
-        "Action package:shake" === "https://hackage.haskell.org/package/shake/docs/Development-Shake.html#t:Action"
-        "Action package:shake set:stackage" === "https://hackage.haskell.org/package/shake/docs/Development-Shake.html#t:Action"
+        "Action package:shake" ===
+          "https://hackage.haskell.org/package/shake/docs/Development-Shake.html#t:Action"
+        "Action package:shake set:stackage" ===
+          "https://hackage.haskell.org/package/shake/docs/Development-Shake.html#t:Action"
         "map -package:base" ==$ \x -> not $ "/base/" `isInfixOf` x
         "<>" === hackage "base/docs/Data-Monoid.html#v:-60--62-"
         "Data.Set.insert" === hackage "containers/docs/Data-Set.html#v:insert"
@@ -155,9 +177,11 @@ action_search_test sample database = testing "Action.Search.search" $ withSearch
         "sparkle" === hackage "sparkle" -- library without Hoogle docs
         "weeder" === hackage "weeder" -- executable in Stackage
         "supero" === hackage "supero"
-
         let tags = completionTags store
-        let asserts b x = if b then putChar '.' else error $ "Assertion failed, got False for " ++ x
+        let asserts b x =
+              if b
+                then putChar '.'
+                else error $ "Assertion failed, got False for " ++ x
         asserts ("set:haskell-platform" `elem` tags) "set:haskell-platform `elem` tags"
         asserts ("author:Neil-Mitchell" `elem` tags) "author:Neil-Mitchell `elem` tags"
         asserts ("package:uniplate" `elem` tags) "package:uniplate `elem` tags"
